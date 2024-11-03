ceaf3e390a01c7ba1719dbf5728ae7c28e3c0f58
Accel <denisignatenko2006@gmail.com>
2024-11-03 21:17:52 +0700

    pw

---
+++ practic5/Helper.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using practic5.Models;
+
+namespace practic5
+{
+    internal class Helper
+    {
+        private static furniture_centreEntities _context;
+
+        public static furniture_centreEntities GetContext()
+        {
+            if (_context == null)
+            {
+                _context = new furniture_centreEntities();
+            }
+            return _context;
+        }
+
+        public void CreateUser(User user)
+        {
+            _context.User.Add(user);
+            _context.SaveChanges();
+        }
+
+        private void UpdateUser(User user)
+        {
+            _context.Entry(user).State = System.Data.Entity.EntityState.Modified;
+            _context.SaveChanges();
+        }
+
+        public void RemoveUser(int idUser)
+        {
+            var users = _context.User.Find(idUser);
+            _context.User.Remove(users);
+            _context.SaveChanges();
+        }
+
+        public List<User> FiltrUsers()
+        {
+            return _context.User.OrderBy(x => x.Login.StartsWith("M") || x.Login.StartsWith("A")).ToList();
+        }
+
+        public List<User> SortUsers()
+        {
+            return _context.User.OrderBy(x => x.Login).ToList();
+        }
+
+        public string GetUserTypes(Employee employee)
+        {
+            
+            return employee.Job_title.Name.ToString();
+        }
+    }
+}
---
+++ practic5/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HashPassword;
+using practic5.Models;
+
+namespace practic5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Helper helper = new Helper();
+            Helper.GetContext();
+            Hash hash = new Hash();
+
+            Console.WriteLine("Введите логин: ");
+            string login = Console.ReadLine();
+            Console.WriteLine("Введите пароль: ");
+            string password = Console.ReadLine();
+            Console.WriteLine("Введите ID сотрудника: ");
+            Int64 idEmployee = Int64.Parse(Console.ReadLine());
+
+            string hashPassw = hash.hashPassword(password);
+            Console.WriteLine($"Хэшированный пароль: {hashPassw}");
+
+            User user = new User();
+            user.Login = login;
+            user.Password = hashPassw;
+            user.Employee = idEmployee;
+
+            helper.CreateUser(user);
+
+            Console.ReadLine();
+        }
+    }
+}
